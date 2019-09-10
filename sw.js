0a85d08829583f4534e1e7d2919143ccdab7abe1
ingrwf <ingrwf@cpg-453069.cepegra2008.lan>
2019-09-10 12:02:31 +0200

    fisrt commit

---
+++ sw.js
@@ -0,0 +1,31 @@
+// CECI EST MON SERVICE WORKER //
+
+const staticAssets = ["/", "/index.html", "/css/style.css", "/js/main.js"];
+
+/* INSTALLATION */
+self.addEventListener("install", ev => {
+  console.log("service worker installed !");
+
+  //CASHING RESSOURCES STATIQUES
+  ev.waitUntil(
+    caches.open("static_V1").then(cache => {
+      cache.addAll(staticAssets);
+      console.log("ressources mise en cache");
+    })
+  );
+});
+
+/* ACTIVATION */
+self.addEventListener("activate", ev => {
+  console.log("Service worker activé");
+});
+
+/* FETCH */
+self.addEventListener("fetch", ev => {
+  console.log("Fetch Request: ", ev.request);
+  ev.respondWith(
+    caches.match(ev.request).then(result => {
+      return result || fetch(ev.request).catch(() => {});
+    })
+  );
+});
